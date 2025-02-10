da3f1df877e0d2eda9f59bb00331611429e5e441
Ishya <n210085@gmail.com>
2025-02-10 16:05:49 +0530

    header file is modified

--- blueprint/components/my_components/Header.jsx
+++ blueprint/components/my_components/Header.jsx
@@ -1,1 +1,42 @@
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { LogIn, Menu } from "lucide-react";
+
+const Header = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+
+
+  return (
+    <header className="sticky top-0 z-50 bg-white shadow-sm">
+      <div className="container mx-auto px-4 flex items-center justify-between py-4">
+        <div className="flex items-center">
+          <Image
+            src="/logo.png"
+            width={150}
+            height={80}
+            alt="Blue Print Logo"
+            className="object-contain"
+          />
+        </div>
+
+        
+        <div className="md:hidden">
+          <Button
+            variant="ghost"
+            size="icon"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          >
+            <Menu />
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
 
